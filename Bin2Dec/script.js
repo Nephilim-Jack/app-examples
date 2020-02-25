9ef56e139d6d7548a551f5f9737929b3fae878c0
Nephilim-Jack <ronaldo_rogerio@yahoo.com.br>
2020-02-25 19:51:06 -0300

    final commit, everything working

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -1,7 +1,51 @@
-// $(document).ready()
 var userInput = $('#binInput')
 var decimalOutput = $('#decOutput')
 
-// Only accept 0 and 1
+
+
+// Only accepts 0 and 1
+function isBinary(eventNum) {
+    if (eventNum >= 48 && eventNum <= 49) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Reverse String
+function reversed(string) {
+    var stringList = string.split('')
+    stringList = stringList.reverse()
+
+    var newString = stringList.join('')
+    return newString
+}
 
 // Transform to Decimal
+function makeDecimal() {
+    var decValue = 0
+
+    for (var i = parseInt(userInput.val().length); i >= 0; i--) {
+
+        intVal = parseInt(reversed(userInput.val())[i])
+        if (!intVal) {
+            intVal = 0
+        }
+        console.log(intVal)
+
+        decValue += intVal * (2 ** i)
+    }
+    decimalOutput.text(decValue)
+}
+
+// add event listener
+userInput.keypress(function (event) {
+    if (!isBinary(event.which)) {
+        event.preventDefault()
+    }
+})
+
+userInput.keyup(function (pevent) {
+    makeDecimal()
+})
+
